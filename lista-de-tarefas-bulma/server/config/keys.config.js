fed15bb500182f8b8e17f4975d74b5498dd9b969
unknown <luizfelipe.smiranda@gmail.com>
2019-06-20 14:05:24 -0300

    Ajuste na configuração da conexão com a base de dados na aplicação Lista de Tarefas

--- lista-de-tarefas-bulma/server/config/keys.config.js
+++ lista-de-tarefas-bulma/server/config/keys.config.js
@@ -1,9 +1,9 @@
-const conexaoDB = {
-    host: 'localhost',
-    porta: '27017',
-    base: 'db_lista-de-tarefas',
-}
+const conexaoLocalDbUrl = 'mongodb://localhost:27017/db_lista-de-tarefas';
+
+const conexaoNuvemDbUrl = 'mongodb+srv://nodeexperiences:dtp_s3cr3t@clustermongodb-nodeexperiences-elhch.mongodb.net/db_lista-de-tarefas';
+
+const conexaoDbUrl = conexaoNuvemDbUrl;
 
 module.exports = {
-    mongodbURI: 'mongodb://' + conexaoDB.host + ':' + conexaoDB.porta + '/' + conexaoDB.base,
+    mongodbURI: conexaoDbUrl,
 }
